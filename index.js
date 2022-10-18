7273eaea4c23fee43daf5e7ffe0ce01fd9b296af
JulisNovais <julietedev@gmail.com>
2022-10-18 20:49:46 -0300

    add metodo post

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 
 const app = express()
 
-const hostname = '192.168.2.151'
+const hostname = 'localhost'
 
 const port = 5000
 
 const books = [
-    {_id: 1, title: 'O dia do Curinga', autor: 'Jostein Gaarden', favorito: true},
-    {_id: 2, title: 'O mundo de sofia', autor: 'Jostein Gaarden', favorito: true},
-    {_id: 3, title: 'A casa', autor: 'Raquel de Queiroz', favorito: false}
+    { _id: 1, title: 'O dia do Curinga', autor: 'Jostein Gaarden', favorito: true },
+    { _id: 2, title: 'O mundo de sofia', autor: 'Jostein Gaarden', favorito: true },
+    { _id: 3, title: 'A casa', autor: 'Raquel de Queiroz', favorito: false }
 ]
 
 app.use(
@@ -30,7 +30,23 @@ app.get('/html', (req, res) => {
 })
 
 app.get('/books', (req, res) => {
-    res.send(books.filter(i => !i.favorito))
+    res.send(books.filter(i => i.favorito))
+})
+
+app.post('/books', (req, res) => {
+    books.push(req.body)
+
+    res.send(req.body)
+})
+
+app.get('/books/:id', (req, res) => {
+    
+    const book = books.find((book) => book._id == req.params.id)
+
+    if (book)
+        res.send(book)
+    else
+        res.sendStatus(404)
 })
 
 app.listen(port, hostname, () => console.log(`http://${hostname}:${port}`))
